c2813666be0fe824fd9ad90e2e0caa776a8092b7
Jimmy Qiu <qiuj@STTARR-067.rmpuhn.ca>
2014-05-27 11:55:05 -0400

    4. comment should refer to inverse

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@
 ##    1. set the value of the matrix
 ##    2. get the value of the matrix
 ##    3. set the value of the inverse
-##    4. get the value of the matrix
+##    4. get the value of the inverse
 ##
 ## Args:
 ##    x: an invertible matrix
