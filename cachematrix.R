71a82c03304deaf8b8b21382db24e271ecee1adf
Jimmy Qiu <jqvroom@gmail.com>
2014-06-19 19:00:13 -0400

    Style follow Google's R style guide

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Matrix inversion can be expensive to compute repeatedly, particularly for large matrices.
-## The pair of functions below cache the inverse of an invertible matrix.
-
-## Write a short comment describing this function
-## Creates a special vector - list containing functions to:
-##    1. set the value of the matrix
-##    2. get the value of the matrix
-##    3. set the value of the inverse
-##    4. get the value of the matrix
-##
-## Args:
-##    x: an invertible matrix
-##
-## Returns:
-##    list containing the set / get functions
+## I've used Google's R style guide as a style reference:
+## https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## Creates a special vector - list containing functions to:
+  ##    1. set the value of the matrix
+  ##    2. get the value of the matrix
+  ##    3. set the value of the inverse
+  ##    4. get the value of the matrix
+  ##
+  ## Args:
+  ##    x: an invertible matrix
+  ##
+  ## Returns:
+  ##    list containing the set / get functions
+
   inv <- NULL
-  set <- function(y){
+  set <- function(y) {
     x <<- y
     inv <<- NULL
   }
@@ -31,19 +28,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## Write a short comment describing this function
-## Calculate the inverse of the matrix stored from calling makeCacheMatrix
-##
-## Args:
-##    x: list of set / get functions from makeCacheMatrix
-##
-## Returns:
-##    inverse of matrix stored in x
-
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  ## Calculate the inverse of the matrix stored from calling makeCacheMatrix
+  ##
+  ## Args:
+  ##    x: list of set / get functions from makeCacheMatrix
+  ##
+  ## Returns:
+  ##    inverse of matrix stored in x
+
   inv <- x$getinverse()
-  if(!is.null(inv)){
+  if (!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
